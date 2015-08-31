fef57d0321ca67fdbf96b9e23cbfd2cdaa8d230c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-31 13:26:23 +0900

    D-1(m)
    s-1#1
    ---
    1. setup
    
    	==> done

---
+++ main/D-1/D-1_s-1.c
@@ -0,0 +1,24 @@
+#ifndef STDIO_H_
+#define STDIO_H_
+#include <stdio.h>
+#endif
+
+#ifndef STDLIB_H_
+#define STDLIB_H_
+#include <stdlib.h>
+#endif
+
+//#include "wave.h"
+
+#include "../../utils/utils.h"
+
+int main(int argc, char *argv[])
+//int main(void)
+{
+
+
+	printf("[%s:%d] done\n", __FILE__, __LINE__);
+
+
+	return 0;
+}
